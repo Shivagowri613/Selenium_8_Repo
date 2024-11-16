e75f03fede47c543b6e999bbccab974ee3667b92
Gowthami <Gowthami@DESKTOP-KIRHED7>
2024-11-16 10:08:58 +0530

    Github Pull request

---
+++ AutomationFramework_8/src/test/java/Gowthami/Github.java
@@ -0,0 +1,13 @@
+package Gowthami;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Github {
+	
+	@Test
+	public void addForkAndPull() {
+		Reporter.log("welcome", true);
+	}
+
+}
